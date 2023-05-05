837e12463b97d90675f87564a116f5c8f33b2351
Wendesday36 <bertazoeanna@gmail.com>
2023-05-05 12:37:30 +0200

    leila kodja

--- Termek.js
+++ Termek.js
@@ -1,21 +1,31 @@
-import { kedvencLista } from "./Kedvenc.js";
-class Termek{
-    #allapot
-    constructor(tartalmHely, adat) {
+class Termek {
+  constructor(szuloElem, elem) {
+    this.szuloElem = szuloElem;
+    this.elem = elem;
+    szuloElem.append(
+      `<div class="card"><div class="card-header">${elem.cim}</div><div class="card-body">${elem.éve}</div><button type="button">Kedvenc</button></div>`
+    );
+    /*
+<div class="card">
+  <div class="card-header">Header</div>
+  <div class="card-body">Content</div>
+  <div class="card-footer">Footer</div>
+</div>
+    */
 
-        this.tartalmHely = this.tartalmHely;
-        this.adat=adat
-        this.#allapot = true;
-        console.log(adat)
-        tartalmHely.append(`<div><h2>${adat.cim}</h2> <p> ${adat.éve}</p> <button  type="button">Click Me!</button></div>`)
-        const gomb = $("div:last-child button")    
-        gomb.on("click",()=>{
-           /*  console.log(adat) */
-            adat.push(kedvencLista)
-            console.log(kedvencLista)
-        }
-        )
+    const BTTN = $(`div:last-child button`);
 
+    BTTN.on("click", () => {
+      this.esemenyTrigger();
+    });
+  }
+
+  esemenyTrigger() {
+    const esemeny = new CustomEvent("kedvenc", {
+      detail: this,
+    });
+    window.dispatchEvent(esemeny);
+  }
 }
-}
+
 export default Termek;
--- Termekek.js
+++ Termekek.js
@@ -1,13 +1,19 @@
-import { TERMEKLISTA } from "./adatok.js";
 import Termek from "./Termek.js";
-class Termekek{
-    constructor(){
-        const tartalmHely=$("article");
-        for (let index = 0; index < TERMEKLISTA.length; index++) {
-            const elem = new Termek(tartalmHely ,TERMEKLISTA[index]);
-            
-          }
-          
+import { TERMEKLISTA } from "./adatok.js";
+
+class Termekek {
+  #kedvencek = [];
+  constructor() {
+    const szuloElem = $("article");
+    for (let index = 0; index < TERMEKLISTA.length; index++) {
+      const t1 = new Termek(szuloElem, TERMEKLISTA[index]);
     }
+
+    $(window).on("kedvenc", (event) => {
+      this.#kedvencek.push(event.detail);
+      console.log(this.#kedvencek)
+    });
+  }
 }
+
 export default Termekek;
